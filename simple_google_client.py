712f2c303a2c19fa4d57a8ff9b4ed9d3da0d84e0
malinborn <rikkeisy@gmail.com>
2024-07-12 11:00:54 +0300

    Added other services building, drive service

--- simple_google_client.py
+++ simple_google_client.py
@@ -6,28 +6,29 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 class GoogleClient:
     def __init__(self, credentials_path: str, table_id: str = ""):
-        self._service = GoogleClient.build_service(credentials_path)
+        self.spreadsheet = GoogleClient.build_service(credentials_path)
+        self.drive = GoogleClient.build_service(credentials_path, "drive", "v3")
         self.main_table_id = table_id
 
     @staticmethod
-    def build_service(credentials_path):
+    def build_service(credentials_path, service_name='sheets', api_version='v4'):
         credentials = ServiceAccountCredentials.from_json_keyfile_name(credentials_path,
                                                                        ['https://www.googleapis.com/auth/spreadsheets',
                                                                         'https://www.googleapis.com/auth/drive']
                                                                        )
         http_auth = credentials.authorize(httplib2.Http())
-        service = apiclient.discovery.build('sheets', 'v4', http=http_auth)
+        service = apiclient.discovery.build(service_name, api_version, http=http_auth)
         return service
 
     def get_values(self, spreadsheet_id: str, range: str) -> dict:
-        request = self._service.spreadsheets().values().get(spreadsheetId=spreadsheet_id,
-                                                                range=range)
+        request = self.spreadsheet.spreadsheets().values().get(spreadsheetId=spreadsheet_id,
+                                                               range=range)
         response = request.execute()
         return response
 
     def add_sheet(self, spreadsheet_id: str, request: dict[str, list[dict[str, dict[str, dict[str, str]]]]]) -> dict:
-        request = self._service.spreadsheets().batchUpdate(spreadsheetId=spreadsheet_id,
-                                                           body=request)
+        request = self.spreadsheet.spreadsheets().batchUpdate(spreadsheetId=spreadsheet_id,
+                                                              body=request)
 
         response = request.execute()
         return response
@@ -36,10 +37,10 @@ class GoogleClient:
     def append_values(self, spreadsheet_id: str, range: str,
                       data: dict[str, list[list[str | int | float | bool | None]] | str],
                       value_input_option: str) -> dict:
-        request = self._service.spreadsheets().values().append(spreadsheetId=spreadsheet_id,
-                                                               range=range,
-                                                               body=data,
-                                                               valueInputOption=value_input_option)
+        request = self.spreadsheet.spreadsheets().values().append(spreadsheetId=spreadsheet_id,
+                                                                  range=range,
+                                                                  body=data,
+                                                                  valueInputOption=value_input_option)
         response = request.execute()
         return response
 
@@ -48,9 +49,27 @@ if __name__ == "__main__":
     import os
     from pprint import pprint
 
-    google_service = GoogleClient(os.getenv("GOOGLE_TOKEN_PATH"))
+    google = GoogleClient(os.getenv("GOOGLE_TOKEN_PATH"))
 
-    test_table_id = "1acz1eBGEBwd_qPvJLLQGzj_wcRek8jrR66ceWUIw0n0"
+    file_id = 'your_file_id'
+
+    permissions = google.drive.permissions().list(fileId=file_id).execute()
+    pprint(permissions)
+
+    # for permission in permissions.get('permissions', []):
+    #     print(f"ID: {permission['id']}, Role: {permission['role']}, Type: {permission['type']}")
+    #
+    # for permission in permissions.get('permissions', []):
+    #     print(f"ID: {permission['id']}")
+    #     print(f"Type: {permission['type']}")
+    #     print(f"Role: {permission['role']}")
+    #     print(f"Email Address: {permission.get('emailAddress', 'N/A')}")
+    #     print(f"Domain: {permission.get('domain', 'N/A')}")
+    #     print(f"AllowFileDiscovery: {permission.get('allowFileDiscovery', 'N/A')}")
+    #     print(f"DisplayName: {permission.get('displayName', 'N/A')}")
+    #     print(f"PhotoLink: {permission.get('photoLink', 'N/A')}")
+    #     print("------")
+
     # pprint(google_service.get_values(test_table_id, "Sheet1!A1:C4"))
 
     # data_to_append = {
